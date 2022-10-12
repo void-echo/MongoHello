288abdd4b958b004d4304744d0ecdd2cee09304f
void-echo <beihai0616@gmail.com>
2022-10-12 09:14:00 +0800

    initial

---
+++ src/main/java/com/echo/mongohello/MongoHelloApplication.java
@@ -0,0 +1,13 @@
+package com.echo.mongohello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoHelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/echo/mongohello/MongoHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echo.mongohello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
